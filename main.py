9ebc323ff8fd4bf36d0b6b078492fd62377ccc86
Henry688 <henry.yang@arcor.de>
2026-01-16 21:08:55 +0100

    Modify main to use group class

--- main.py
+++ main.py
@@ -13,16 +13,29 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    #Play Loop
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
-        player.draw(screen)
+        
+        for object in updatable:
+            object.update(dt)
+        
+        for object in updatable:
+            object.draw(screen)
+
         pygame.display.flip()
 
         clock.tick(60)
